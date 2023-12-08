337c35961b9577943f3d0f1b057a752c39a5dc36
Usman4772 <142718220+Usman4772@users.noreply.github.com>
2023-12-08 15:15:33 +0500

    Files uploaded

---
+++ ArrayData.js
@@ -0,0 +1,94 @@
+let array=[
+    {
+            imgSrc:"https://images.unsplash.com/photo-1581655353564-df123a1eb820?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dCUyMHNoaXJ0c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            name:"White shirt",
+            category:"Shirts",
+            
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1504117809472-d99bf528ea44?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmxhY2slMjBwYW50fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+            name:"Black pant",
+            category:"pants"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1625585445301-676203ae67fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJsdWUlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            name:"blue shirt",
+            category:"shirts"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1525326006910-384ca07920d1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGluayUyMHNoaXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+            name:"pink Shirt",
+            category:"shirts"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1593766821405-f605e0f9535f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNyaWNrZXQlMjBiYXR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+            name:"Cricket Bat",
+            category:"Sports"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1531415074968-036ba1b575da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y3JpY2tldCUyMGJhdHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            name:"Cricket Ball",
+            category:"Sports"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1562232723-6949c130734d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9iaWxlJTIwY2hhcmdlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            name:"Charger",
+            category:"Electronics"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1581541234269-03d5d8576c0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2hhaXJzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+            name:"Chairs",
+            category:"Furniture"
+    
+    },
+    {
+            imgSrc:"https://plus.unsplash.com/premium_photo-1671269943825-e45b177add8f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmVkc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            name:"Beds",
+            category:"Furniture"
+    
+    },
+    {
+            imgSrc:"https://media.istockphoto.com/id/1445237941/photo/closeup-on-electric-floor-standing-fan-in-the-modern-living-room-in-sunny-hot-summer-day.webp?s=1024x1024&w=is&k=20&c=l7XhnKrqJVIAAAyytnGsfAw9d_k98vuupgRVASmfB4Y=",
+            name:"Fans",
+            category:"Electronics"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+            name:"Laptops",
+            category:"Electronics"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1510557880182-3d4d3cba35a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+            name:"Mobiles",
+            category:"Electronics"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1610553556003-9b2ae8ef1b8e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmlrZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+            name:"Bike",
+            category:"Automobiles"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+            name:"Car",
+            category:"Automobiles"
+    
+    },
+    {
+            imgSrc:"https://images.unsplash.com/photo-1522338140262-f46f5913618a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1376&q=80",
+            name:"Hair Dryer",
+            category:"Electronics"
+    
+    },
+    ]
+    export default array;
